dc3f8d971f2ee8fc68da10e3423a7e8b90527ba2
vmanav <zmanav.1999@gmail.com>
2021-01-25 01:00:41 +0530

    basic socket-client setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from "socket.io-client";
+
 import './App.css';
 
+const ENDPOINT = "http://127.0.0.1:5000";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [res, setRes] = useState("");
+
+  useEffect(() => {
+    console.log("useEff CALLED")
+    const socket = socketIOClient(ENDPOINT,
+    //    {
+    //   withCredentials: true,
+    // }
+    );
+    socket.on("FromAPI", data => {
+      setRes(data);
+    });
+  }, []);
+
+  return (<p>It's <time dateTime={res}>{res}</time></p>);
 }
 
 export default App;
