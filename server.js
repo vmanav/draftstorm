b5d9ff9e880820cde53176b9aa0591e8f018b738
vmanav <zmanav.1999@gmail.com>
2021-10-03 01:14:44 +0530

    mover server files to root directory

---
+++ server.js
@@ -0,0 +1,115 @@
+const utils = require('./utils');
+const express = require('express')
+const socket = require('socket.io')
+const http = require('http')
+const path = require("path")
+
+const PORT = process.env.PORT || 5000;
+
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+const server = http.createServer(app);
+
+const io = socket(server,
+  {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  }
+);
+
+const {
+  addUser, removeUser, getUser, showUsers, getRooms,
+} = require('./users');
+
+app.get('/rooms', (req, res) => {
+  const rooms = getRooms();
+  console.log("rooms : ", rooms);
+  res.send({ rooms: rooms });
+})
+
+io.on('connection', (socket) => {
+
+  // console.log("User Connected : ", socket.id);
+  socket.on(utils.MESSAGE_TYPES.JOIN_ROOM, function ({ userName, roomName }, callback) {
+
+    const { error, newUser } = addUser({ socketId: socket.id, userName, roomName });
+    console.log("error : ", error);
+    console.log("newUser : ", newUser);
+    showUsers();
+
+    if (error) {
+      socket.emit(
+        utils.MESSAGE_TYPES.ERROR, {
+        text: error,
+        type: utils.MESSAGE_CLASSES.RED_FLAG,
+      }
+      );
+      return;
+    }
+    socket.join(newUser.roomName);
+    socket.emit(
+      utils.MESSAGE_TYPES.NOTIFICATION, {
+      text: `You have joined the Room : ${newUser.roomName}`,
+      type: utils.MESSAGE_CLASSES.GREEN_FLAG,
+    }
+    );
+    socket.broadcast.to(newUser.roomName).emit(
+      utils.MESSAGE_TYPES.NOTIFICATION, {
+      text: `${newUser.userName} has joined the Room.`,
+      type: utils.MESSAGE_CLASSES.GREEN_FLAG,
+    }
+    );
+  })
+
+  // Message Functionality #TBD
+  // socket.on("sendMsg", (message, callback) => {
+  //   const currUser = getUser(socket.id);
+  //   io.to(currUser.roomName).emit('msg', { userName: currUser.userName, text: message })
+  //   callback();
+  // })
+
+  socket.on(utils.MOUSE_EVENTS.CLIENT_DOWN, (payload) => {
+
+    console.log("c_mouse_down payload RCVD : ", payload)
+    const currUser = getUser(socket.id);
+    io.to(currUser.roomName).emit(utils.MOUSE_EVENTS.SERVER_DOWN, payload);
+  })
+
+  socket.on(utils.MOUSE_EVENTS.CLIENT_MOVE, (payload) => {
+
+    console.log("c_mouse_move payload RCVD : ", payload)
+    const currUser = getUser(socket.id);
+    io.to(currUser.roomName).emit(utils.MOUSE_EVENTS.SERVER_MOVE, payload);
+  })
+
+  socket.on('disconnect', (reason) => {
+
+    console.log(reason, '=> Disconnected : ', socket.id);
+    const leavingUser = removeUser(socket.id);
+    showUsers();
+    console.log("Leaving User : ", leavingUser);
+    if (leavingUser) {
+      socket.broadcast.to(leavingUser.roomName).emit(
+        utils.MESSAGE_TYPES.NOTIFICATION, {
+        text: `${leavingUser.userName} has Left the Room.`,
+        type: utils.MESSAGE_CLASSES.RED_FLAG,
+      }
+      );
+    }
+  });
+})
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+server.listen(PORT, () => {
+  console.log("Listening on : http://localhost:" + PORT);
+})
---
+++ users.js
@@ -0,0 +1,70 @@
+let users = [];
+
+let roomParticipantMap = {};
+
+function addUser({ socketId, userName, roomName }) {
+  // Transform userName and roomName into one word., e.g. :Manav Verma => manavverma
+
+  userName = userName.trim().toLowerCase();
+  roomName = roomName.trim().toLowerCase();
+  console.log("INCOMING : ", socketId, userName, roomName);
+
+  const existingUser = users.find((user) => user.roomName === roomName && user.userName === userName);
+
+  // if (!userName || !roomName) return { error: 'Username and roomName are required.' };
+  if (existingUser) {
+    // Emit Error Message when Username is occupied
+    return { error: 'Username is taken.' };
+  }
+  const newUser = { socketId, userName, roomName };
+  console.log("newUser", newUser);
+  users.push(newUser);
+  addRoom(roomName);
+  return { newUser };
+}
+
+const removeUser = (socketId) => {
+  const index = users.findIndex((user) => user.socketId === socketId);
+
+  if (index !== -1) {
+    const { roomName } = users[index];
+    removeRoom((roomName))
+    return users.splice(index, 1)[0];
+  }
+}
+
+const getUser = (socketId) => users.find((user) => user.socketId === socketId);
+
+const getUsersInRoom = (roomName) => users.filter((user) => user.roomName === roomName);
+
+const showUsers = () => {
+  console.log("All Users : ")
+  users.map((user) => {
+    console.log(user);
+    return user;
+  });
+}
+
+const getRooms = () => Object.keys(roomParticipantMap);
+
+const addRoom = (roomName) => {
+  if (roomParticipantMap[roomName]) {
+    roomParticipantMap[roomName] += 1;
+  } else {
+    roomParticipantMap[roomName] = 1;
+  }
+};
+
+const removeRoom = (roomName) => {
+  if (roomParticipantMap[roomName]) {
+    if (roomParticipantMap[roomName] > 1) {
+      roomParticipantMap[roomName] -= 1;
+    } else {
+      delete roomParticipantMap[roomName];
+    }
+  }
+};
+
+module.exports = {
+  addUser, removeUser, getUser, getUsersInRoom, showUsers, getRooms,
+};
